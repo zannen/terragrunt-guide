ded4e386fe521b97561afc0e3146229bbdd4de94
ZanNen <github3746@ashleygc.co.uk>
2025-10-01 13:15:56 +0100

    Split env into Dev and Prod

--- live/bucket-sync.sh
+++ live/bucket-sync.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# Get the bucket name
-bucket_name="$(tofu output -raw s3_bucket_name)"
+top_level="$(git rev-parse --show-toplevel)"
 
-# Go to repo top level
-cd "$(git rev-parse --show-toplevel)"
+for env in dev prod ; do
+	echo "=== $env ==="
 
-# Sync the static dir with the bucket
-cd dist/static
-aws s3 sync . "s3://${bucket_name}/"
+	# Go to repo top level
+	cd "$top_level/live"
+
+	bucket_name="$(tofu output -raw "${env}_s3_bucket_name")"
+
+	# Sync the static dir with the bucket
+	cd "$top_level/dist/static/$env"
+	aws s3 sync . "s3://$bucket_name/"
+done
--- live/moved.tf
+++ live/moved.tf
@@ -1,54 +1,54 @@
 moved {
-  from = aws_dynamodb_table.asset_metadata
-  to   = module.ddb.aws_dynamodb_table.asset_metadata
+  from = module.ddb.aws_dynamodb_table.asset_metadata
+  to   = module.prod.module.ddb.aws_dynamodb_table.asset_metadata
 }
 
 moved {
-  from = aws_iam_policy.lambda_basic_execution
-  to   = module.iam.aws_iam_policy.lambda_basic_execution
+  from = module.iam.aws_iam_policy.lambda_basic_execution
+  to   = module.prod.module.iam.aws_iam_policy.lambda_basic_execution
 }
 
 moved {
-  from = aws_iam_policy.lambda_dynamodb
-  to   = module.iam.aws_iam_policy.lambda_dynamodb
+  from = module.iam.aws_iam_policy.lambda_dynamodb
+  to   = module.prod.module.iam.aws_iam_policy.lambda_dynamodb
 }
 
 moved {
-  from = aws_iam_policy.lambda_s3_read
-  to   = module.iam.aws_iam_policy.lambda_s3_read
+  from = module.iam.aws_iam_policy.lambda_s3_read
+  to   = module.prod.module.iam.aws_iam_policy.lambda_s3_read
 }
 
 moved {
-  from = aws_iam_role.lambda_role
-  to   = module.iam.aws_iam_role.lambda_role
+  from = module.iam.aws_iam_role.lambda_role
+  to   = module.prod.module.iam.aws_iam_role.lambda_role
 }
 
 moved {
-  from = aws_iam_role_policy_attachment.lambda_basic_execution
-  to   = module.iam.aws_iam_role_policy_attachment.lambda_basic_execution
+  from = module.iam.aws_iam_role_policy_attachment.lambda_basic_execution
+  to   = module.prod.module.iam.aws_iam_role_policy_attachment.lambda_basic_execution
 }
 
 moved {
-  from = aws_iam_role_policy_attachment.lambda_dynamodb
-  to   = module.iam.aws_iam_role_policy_attachment.lambda_dynamodb
+  from = module.iam.aws_iam_role_policy_attachment.lambda_dynamodb
+  to   = module.prod.module.iam.aws_iam_role_policy_attachment.lambda_dynamodb
 }
 
 moved {
-  from = aws_iam_role_policy_attachment.lambda_s3_read
-  to   = module.iam.aws_iam_role_policy_attachment.lambda_s3_read
+  from = module.iam.aws_iam_role_policy_attachment.lambda_s3_read
+  to   = module.prod.module.iam.aws_iam_role_policy_attachment.lambda_s3_read
 }
 
 moved {
-  from = aws_lambda_function.main
-  to   = module.lambda.aws_lambda_function.main
+  from = module.lambda.aws_lambda_function.main
+  to   = module.prod.module.lambda.aws_lambda_function.main
 }
 
 moved {
-  from = aws_lambda_function_url.main
-  to   = module.lambda.aws_lambda_function_url.main
+  from = module.lambda.aws_lambda_function_url.main
+  to   = module.prod.module.lambda.aws_lambda_function_url.main
 }
 
 moved {
-  from = aws_s3_bucket.static_assets
-  to   = module.s3.aws_s3_bucket.static_assets
+  from = module.s3.aws_s3_bucket.static_assets
+  to   = module.prod.module.s3.aws_s3_bucket.static_assets
 }
